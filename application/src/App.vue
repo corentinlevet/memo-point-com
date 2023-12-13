bb779b18cf6b7077caca97b8fd290071dd904b0b
corentinlevet <corentin.levet@epitech.eu>
2023-12-13 15:03:49 +0100

    [~] Refactoring architecture

--- application/src/App.vue
+++ application/src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import 'bootstrap/dist/css/bootstrap.min.css';
 </script>
 
 <template>
--- application/src/views/AuthView.vue
+++ application/src/views/AuthView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-  import goServer from '../api/go-server';
+  import goServer from '@/api/go-server';
 
-  import 'bootstrap/dist/css/bootstrap.min.css';
+  import '@/assets/Auth/style.css';
 </script>
 
 <template>
@@ -13,17 +13,17 @@
       </div>
       <div class="inputs">
         <div class="input">
-          <img src="../assets/Auth/person.png" alt="user icon" />
+          <img src="@/assets/Auth/person.png" alt="user icon" />
           <input type="text" v-model="username" placeholder="Username" required />
         </div>
 
         <div id="email-input" style="display: flex;" class="input">
-          <img src="../assets/Auth/email.png" alt="email icon" />
+          <img src="@/assets/Auth/email.png" alt="email icon" />
           <input type="email" v-model="email" placeholder="Email" required />
         </div>
 
         <div class="input">
-          <img src="../assets/Auth/password.png" alt="password icon" />
+          <img src="@/assets/Auth/password.png" alt="password icon" />
           <input type="password" v-model="password" placeholder="Password" required />
         </div>
       </div>
@@ -154,107 +154,3 @@
     }
   }
 </script>
-
-<style scoped>
-  .page-container {
-    margin: 0;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    height: 100vh;
-    overflow: hidden;
-    background: linear-gradient(#ca1111, #d17f03);
-    display: flex;
-    justify-content: center;
-  }
-
-  .auth-container {
-    display: flex;
-    flex-direction: column;
-    margin: auto;
-    background: #fff;
-    width: 600px;
-    border-radius: 20px;
-    min-height: 600px;
-  }
-
-  .auth-header {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 10px;
-    width: 100%;
-    margin-top: 30px;
-  }
-
-  .text {
-    color: #ca1212;
-    font-size: 2rem;
-    font-weight: 700;
-  }
-
-  .underline {
-    width: 60px;
-    height: 5px;
-    background: #ca1212;
-    border-radius: 10px;
-  }
-
-  .inputs {
-    margin-top: 55px;
-    display: flex;
-    flex-direction: column;
-    gap: 25px;
-  }
-
-  .input {
-    display: flex;
-    align-items: center;
-    margin: auto;
-    width: 480px;
-    height: 80px;
-    background: #EAEAEA;
-    border-radius: 5px;
-  }
-
-  .input img {
-    margin: 0px 30px;
-  }
-
-  .input input {
-    height: 50px;
-    width: 400px;
-    background: transparent;
-    border: none;
-    outline: none;
-    color: #797979;
-    font-size: 20px;
-  }
-
-  .submit-container {
-    display: flex;
-    gap: 30px;
-    margin: 20px auto;
-    margin-top: auto;
-    margin-bottom: 30px;
-  }
-
-  .submit {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 220px;
-    height: 60px;
-    color: #fff;
-    background: #f89e17;
-    border-radius: 50px;
-    font-size: 20px;
-    font-weight: 700;
-    cursor: pointer;
-  }
-
-  .gray {
-    background: #EAEAEA;
-    color: #676767;
-  }
-
-</style>
